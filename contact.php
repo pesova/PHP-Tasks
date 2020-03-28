208c93d0f7ad0a35b5aea1369e2feb6863035ba8
pesova <62014292+pesova@users.noreply.github.com>
2020-03-28 08:02:14 -0700

    updated the php

--- contact.php
+++ contact.php
@@ -1,22 +1,23 @@
+<html>
+<body>
 
-Thanks<?php echo $_POST["name"]; ?> for submitting <br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Thanks <?php echo $_POST["name"]; ?> for submitting <br>
+
+Your email address is: <?php echo $_POST["emailAddress"]; ?>
 
 
 <?php
-    if(isset($_POST['Submit'])){
-    $FirstName =$_POST["firstname"];
+    if(isset($_POST["Submit"])){
+    $FirstName =$_POST["name"];
     $Comment =$_POST["comments"];
 
     //open the file and choose the mode
 
-    $OpenFile = fopen($FirstName.txt, "a");
+    $OpenFile = fopen($FirstName, "w");
     fwrite($OpenFile, $Comment);
 
-    //close the file
-
-    fclose($fh);
     }
 ?>
 </body>
 </html>
+
